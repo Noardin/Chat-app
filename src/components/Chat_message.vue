a71f40bf42320a264cf0b1af83c2a48dc4e64c1a
Noardin <triksiand@gmail.com>
2019-05-13 16:29:58 +0200

    open modal

--- src/components/Chat_message.vue
+++ src/components/Chat_message.vue
@@ -114,7 +114,7 @@
             },
 
             upravit(){
-              this.$parent.$emit('OpenMessageChangeModal', {message:this.message, id:this.message.id})
+              this.$parent.$emit('OpenMessageChangeModal', {message:this.message.message, id:this.message.id})
             },
 
             vymazat(){
