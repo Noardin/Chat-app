6238ac96717860cb7ed207bd5be1a165c7606349
Noardin <triksiand@gmail.com>
2019-05-13 16:21:21 +0200

    open modal

--- src/api/index.js
+++ src/api/index.js
@@ -12,8 +12,8 @@ export function postNewMessage (message, jwt) {
   return axios.post(URL+'Messages', message,  {headers: { Authorization: jwt } })
 }
 
-export function update(jwt, reactions) {
-    return axios.get(URL+'update_userdata', reactions, {headers: { Authorization: jwt } })
+export function update(jwt) {
+    return axios.get(URL+'update_userdata', {headers: { Authorization: jwt } })
 
 }
 export function update_reactions(jwt, reactions) {
